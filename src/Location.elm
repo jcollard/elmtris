66cf8cca11c51ef6b089474fd42c03f7075b40cf
jcollard <josephmcollard@gmail.com>
2014-11-28 12:18:03 -0500

    Removed old source files.

--- src/Location.elm
+++
@@ -1,4 +0,0 @@
-module Location where
-
--- A location is an (x, y) position
-type Location = (Int, Int)
--- src/Util.elm
+++
@@ -1,9 +0,0 @@
-module Util where
-
--- Creates an infinite list of the specified element
-repeat : a -> [a]
-repeat a = a::(repeat a)
-
-replicate : Int -> a -> [a]
-replicate n a =
-  if n <= 0 then [] else a::(replicate (n-1) a)
